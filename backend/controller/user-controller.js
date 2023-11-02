818ddd7346dd558604769a168c293a756c9ecd0b
anjeljayswal <anjeljayswal@gmail.com>
2023-11-02 15:59:27 +0530

    login

--- backend/controller/user-controller.js
+++ backend/controller/user-controller.js
@@ -27,15 +27,11 @@ export const signup = async (req,res,next)=>{
     return res.status(400).json({message:"User Already Exists! Login Instead"});
    }
    const hashPassword = bcrypt.hashSync(password)
-
-
    const user = new User({
     name,
     email,
     password:hashPassword,
    });
-
-
    try{
     await user.save();
 
@@ -45,3 +41,25 @@ export const signup = async (req,res,next)=>{
 
    return res.status(201).json({user});
 } 
+
+export const login = async(req,res,next)=>{
+    const {email, password}=req.body;
+    let existingUser;
+   try{
+    existingUser = await User.findOne({email})
+
+   }catch(err){
+    return console.log(err);
+   }
+   if(!existingUser){
+    return res.status(404).json({message:"Could not find user By this email"});
+   }
+   const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
+   if(!isPasswordCorrect){
+    return res.status(400).json({message:"Incorrect Password"})
+
+   }
+   return res.status(200).json({message:"Login Successfully"});
+   
+   
+}
